2a2c872190dc094eca911e4057d8d93d10068c2b
viktorasLipinskis <viktoras.lipinskis@gmail.com>
2022-11-05 12:23:06 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
 import Text from "components/Text";
-import "./App.css";
 
 function App() {
   return (
     <div className="bg-violet-600">
       <header className="bg-violet-600">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <Text />
       </header>
     </div>
